34d939f4968b0aa2253a70674cd3c3aa8fb6a6c5
RAJKUMAR <RAJKUMAR@LAPTOP-DD7994OP>
2024-02-27 17:11:52 +0530

    Dev-Update

---
+++ GIT_App1/src/main/java/p1/App.java
@@ -0,0 +1,9 @@
+package p1;
+
+public class App
+{
+   public int sum(int x,int y)
+   {
+	   return(x+y);
+   }
+}
---
+++ GIT_App1/src/main/java/p2/DemoApp.java
@@ -0,0 +1,10 @@
+package p2;
+import p1.*;
+public class DemoApp {
+
+	public static void main(String[] args) {
+		App s=new App();
+		s.sum(10, 11);
+	}
+
+}
